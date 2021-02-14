c85870456cd529a460a6d4bf5b68ff41eeeee04f
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-02-14 23:20:24 +0700

    + jwt strategy, get user decorator

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'quy3n',
+    });
+  }
+  async validate(payload: JwtPayload) {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
