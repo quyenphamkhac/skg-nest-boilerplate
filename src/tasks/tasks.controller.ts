747a0d3bca819f9f5d483e99e320f04c3f156415
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-02-14 23:26:35 +0700

    + guard controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,9 +8,11 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './enums/task-status.enum';
@@ -19,6 +21,7 @@ import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
